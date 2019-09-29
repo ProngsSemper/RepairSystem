bdf11ae8eff65aafa769a2ad38e3521e8b6ed223
idea - your-father <19574765740@qq.com>
2019-09-30 01:49:12 +0800

    添加了Httpclient工具类，可以利用此访问外域资源，例如获取学校登录验证的服务

---
+++ src/main/test/java/NetTest.java
@@ -0,0 +1,21 @@
+import com.alibaba.fastjson.JSONObject;
+import com.google.gson.JsonObject;
+import com.repairsys.util.net.Postman;
+import net.sf.json.util.JSONBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @Author lyr
+ * @create 2019/9/30 1:42
+ * 验证是否通过
+ */
+public class NetTest {
+    @Test
+    public void loginCall() throws IOException {
+        JSONObject jsonObject = Postman.doGet("181543430","yr422525");
+        System.out.println(jsonObject);
+        System.out.println(jsonObject.getString("flag"));
+    }
+}
