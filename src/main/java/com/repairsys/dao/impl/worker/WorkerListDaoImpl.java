33471da321862165b48a1e2aaf6220c498587549
idea - your-father <19574765740@qq.com>
2019-10-02 13:50:06 +0800

    添加了工人查询

--- src/main/java/com/repairsys/dao/impl/worker/WorkerListDaoImpl.java
+++ src/main/java/com/repairsys/dao/impl/worker/WorkerListDaoImpl.java
@@ -2,7 +2,9 @@ package com.repairsys.dao.impl.worker;
 
 import com.repairsys.bean.entity.Worker;
 import com.repairsys.dao.AbstractPageDao;
+import com.repairsys.util.db.JdbcUtil;
 import com.repairsys.util.easy.EasyTool;
+
 
 import java.util.List;
 
@@ -11,8 +13,39 @@ import java.util.List;
  * @create 2019/10/1 22:05
  */
 public class WorkerListDaoImpl extends AbstractPageDao {
+    public enum Column
+    {
+        /**
+         * 用来枚举数据库的列名字
+         */
+        wKey("wKey"),
+        wId("wId"),
+        wName("wName"),
+        wTel("wTel"),
+        wPassword("wPassword"),
+        wMail("wMail"),
+
+
+        ;
+        String name;
+
+        /**
+         * 数据库查询时候需要用到得到列
+         * @param name 数据库的字段名字
+         */
+        Column(String name)
+        {
+            this.name = name;
+        }
+
+    }
+
+
+
+
 
     private static final String GET_WORKER_LIST = "select * from workers limit ?,?";
+    private static final String BASE_QUERY = "select * from workers ";
 
     protected WorkerListDaoImpl(Class clazz) {
         super(clazz);
@@ -35,4 +68,38 @@ public class WorkerListDaoImpl extends AbstractPageDao {
 
        return super.getPageList(GET_WORKER_LIST,targetPage,size);
     }
+
+    /**
+     * 查询对应的工人
+     * @param columns 数据库 worker表的列名字
+     * @param args 数据库的参数
+     * @return 返回worker的bean集合
+     * @deprecated
+     */
+    @Deprecated
+    public List<Worker> getWorkerByCondition(WorkerListDaoImpl.Column[] columns,String[]args)
+    {
+        if(columns==null||args==null)
+        {
+            return null;
+        }
+        if((columns.length!=args.length)||(columns.length<1))
+        {
+            return null;
+        }
+
+        StringBuilder sb = new StringBuilder(BASE_QUERY+" where ");
+        for(int i=0;i<columns.length;++i)
+        {
+            sb.append(columns[i].name +" = "+args[i]);
+        }
+
+
+        return super.selectList(JdbcUtil.getConnection(),sb.toString());
+
+
+    }
+
+
+
 }
--- src/main/java/com/repairsys/util/easy/EasyTool.java
+++ src/main/java/com/repairsys/util/easy/EasyTool.java
@@ -8,6 +8,13 @@ public final class EasyTool {
 
     //分页查询公式  limit (page-1)*size,size
 
+    /**
+     *  形如 select * from table limit ?,?; 的sql语句，给写dao的程序员用的
+     *
+     * @param nextPage 前端点击下一页，的目标页面
+     * @param size 一页要的总的记录数
+     * @return  长度为2的 int数组，第一个数是limit的第一个问号，第二个是 limit的第二个问号
+     */
     public static int[] getLimitNumber(int nextPage,int size)
     {
         if(nextPage<=0)
