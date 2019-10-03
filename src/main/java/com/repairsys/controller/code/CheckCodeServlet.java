515b770e2cb5c4d3de68861e0bff89d240a756c1
idea - your-father <19574765740@qq.com>
2019-10-04 02:26:03 +0800

    已经写完了验证码的功能，修改了过滤器拦截图片，css,js资源的bug,修改了源码垃圾,优化了过滤链处理的方案

--- src/main/java/com/repairsys/controller/code/CheckCodeServlet.java
+++ src/main/java/com/repairsys/controller/code/CheckCodeServlet.java
@@ -20,7 +20,7 @@ import java.util.Random;
  * @Author lyr
  * @create 2019/10/2 19:21
  */
-@WebServlet("/checkCode")
+@WebServlet("/checkCode.png")
 public class CheckCodeServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
