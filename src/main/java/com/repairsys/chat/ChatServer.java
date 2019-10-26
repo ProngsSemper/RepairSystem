f8ec64b892b149a8b6cb52aa11caab4c43b42cdf
idea-your-father <1957476540@qq.com>
2019-10-26 14:58:52 +0800

    添加了 webSocket 的包

---
+++ src/main/java/com/repairsys/chat/ChatServer.java
@@ -0,0 +1,14 @@
+package com.repairsys.chat;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+/**
+ * @Author lyr
+ * @create 2019/10/26 14:35
+ */
+@ServerEndpoint("/chat")
+public class ChatServer {
+}
