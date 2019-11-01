efbc9cc8a0855bcdc98693018be42d129b1fa6aa
Prongs <798237844@qq.com>
2019-11-01 19:23:38 +0800

    修改工人暂无评价显示

--- src/main/java/com/repairsys/dao/impl/worker/WorkerDaoImpl.java
+++ src/main/java/com/repairsys/dao/impl/worker/WorkerDaoImpl.java
@@ -199,7 +199,7 @@ public class WorkerDaoImpl extends BaseDao<Worker> implements com.repairsys.dao.
         double mid = worker.getMid();
         double bad = worker.getBad();
         if (good == 0 && mid == 0 && bad == 0) {
-            return "暂无评价！";
+            return "暂无";
         }
         double sum = good + mid + bad;
         double pre = (good / sum) * 100;
